561ab4a955628c3f4c9bff6b9e9ddec0546b8df7
Jordi Gisbert Ponsoda <jgisbertponsoda@gmail.com>
2018-09-23 19:11:18 +0200

    Añadiendo juego del señor de los anillos del curso de udemy

--- curso-udemy-javascript/palabra-reservada-new.js
+++ curso-udemy-javascript/palabra-reservada-new.js
@@ -1,12 +1,18 @@
 
 
-function Persona() {
+function Persona(nombre,apellido) {
+  this.nombre = nombre;
+  this.apellido = apellido;
+  this.edad = 30;
 
+  this.imprimirPersona = function(){
+    return this.nombre + " " + this.apellido + "(" + this.edad + ")";
+  }
 }
 
+//LA PALABRA RESERVADA NEW CREA A PARTIR DE LA FUNCION ANTERIOR, CREA UN NUEVO OBJETO. SI NO PONEMOS NEW ESTAMOS INVOCANDO A UNA FUNCION NORMAL, NO CREANDO UN OBJETO.
+
+var juan = new Persona("Jordi","Gisbert");
 
 
-var juan = new Persona();
-
-
-console.log(juan);
+console.log(juan.imprimirPersona());
