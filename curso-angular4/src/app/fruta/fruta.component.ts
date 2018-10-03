89b1e24032f76d5d0925ed5cbc786da49b50df6c
Jordi Gisbert Ponsoda <jordi.gisbert.ponsoda@everis.com>
2018-10-03 12:28:26 +0200

    Método constructor y ngOnInit

--- curso-angular4/src/app/fruta/fruta.component.ts
+++ curso-angular4/src/app/fruta/fruta.component.ts
@@ -8,9 +8,32 @@ export class FrutaComponent{
     public nombre_componente = 'Componente de fruta';
     public listado_frutas = 'Naranja, Manzana, Pera y Sandia';
 
-    public nombre:string = 'Jordi Gisbert Ponsoda';
-    public edad:number = 23;
-    public mayorDeEdad:boolean = true;
+    public nombre:string;
+    public edad:number;
+    public mayorDeEdad:boolean;
     public trabajos:Array<string> = ['Carpintero','Albañil', 'Fontanero'];
     public varios:Array<any> = ['Carpintero', 4, 23, 'Fontanero'];
+    comodin:any;
+
+    constructor(){
+        // console.log(this.trabajos); // <-- con this accedemos al objeto FrutaComponent y accedemos al parametro trabajos.
+        this.nombre = 'Jordi Gisbert Ponsoda';
+        this.edad = 23;
+        this.mayorDeEdad = true;
+        this.comodin = 'SI';
+    }
+
+    ngOnInit(){ //PRIMER MÉTODO QUE SE LANZA TRAS EL CONSTRUCTOR, IDEAL PARA LLAMADAS AJAX A SERVICIOS Y COSAS ASÍ
+        this.cambiarNombre(); //NO ES UNA BUENA PRÁCTICA LLAMAR A LOS MÉTODOS DENTRO DEL CONSTRUCTOR, PARA ELLO TENEMOS EL MÉTODO ngOnInit
+        this.cambiarEdad(45);
+        alert(this.nombre + " tiene " + this.edad + " años.");
+    }
+
+    cambiarNombre(nombre){
+        this.nombre = 'Ivan';
+    }
+
+    cambiarEdad(edad){
+        this.edad = edad;
+    }
 }
