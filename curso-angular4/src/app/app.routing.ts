cd78b8e2767332fe48b4fa125fd06190aeebd1d6
Jordi Gisbert Ponsoda <jordi.gisbert.ponsoda@everis.com>
2018-11-15 10:28:25 +0100

    Creando un menú de navegación

--- curso-angular4/src/app/app.routing.ts
+++ curso-angular4/src/app/app.routing.ts
@@ -4,13 +4,17 @@ import { Routes, RouterModule } from '@angular/router';
 //Importar componentes
 import { EmpleadosComponent } from './empleados/empleados.component';
 import { FrutaComponent } from './fruta/fruta.component';
+import { HomeComponent } from './home/home.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 //Definimos una variable constante, es un Array
 const appRoutes: Routes = [
-    {path: '', component:EmpleadosComponent}, //PAGINA HOME, POR DEFECTO
-    {path: 'empleado', component:EmpleadosComponent},
+    {path: '', component:HomeComponent}, //PAGINA HOME, POR DEFECTO
+    {path: 'empleados', component:EmpleadosComponent},
     {path: 'fruta', component:FrutaComponent},
-    {path: '**', component:EmpleadosComponent} //CUANDO DA ERROR APARECE ESTO
+    {path: 'home', component:HomeComponent},
+    {path: 'contacto', component:ContactoComponent},
+    {path: '**', component:HomeComponent} //CUANDO DA ERROR APARECE ESTO
 ];
 
 //Exportamos una constante que es un Array de cualquier tipo
