4874fc5272b018d5812293b4aa947d68dfb7f753
Jordi Gisbert Ponsoda <jordi.gisbert.ponsoda@everis.com>
2018-11-15 10:43:32 +0100

    Rutas y parametros por URL

--- curso-angular4/src/app/app.routing.ts
+++ curso-angular4/src/app/app.routing.ts
@@ -7,13 +7,14 @@ import { FrutaComponent } from './fruta/fruta.component';
 import { HomeComponent } from './home/home.component';
 import { ContactoComponent } from './contacto/contacto.component';
 
-//Definimos una variable constante, es un Array
+//Definimos una variable constante, es un Array, aquí estamos creando rutas
 const appRoutes: Routes = [
     {path: '', component:HomeComponent}, //PAGINA HOME, POR DEFECTO
     {path: 'empleados', component:EmpleadosComponent},
     {path: 'fruta', component:FrutaComponent},
     {path: 'home', component:HomeComponent},
     {path: 'contacto', component:ContactoComponent},
+    {path: 'contacto/:page', component:ContactoComponent},
     {path: '**', component:HomeComponent} //CUANDO DA ERROR APARECE ESTO
 ];
 
--- curso-angular4/src/app/contacto/contacto.component.ts
+++ curso-angular4/src/app/contacto/contacto.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
     selector: 'contacto',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 
 export class ContactoComponent{
     public titulo = "Página de contacto de la web";
+    public parametro;
+
+    constructor(
+        private _route: ActivatedRoute,
+        private _router: Router
+    ){}
+
+    //La flecha es porque estamos usando una función de flecha, en ella no hay que poner la palabra function,
+    //y nos permite acceder a los parametros de la clase, en este caso paramtro y titulo, por ejemplo.
+    ngOnInit(){
+        this._route.params.forEach((params: Params) => {
+            this.parametro = params['page'];
+        });
+    }
 }
