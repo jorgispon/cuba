5f47f8a5b09e0e119b1badc379886b89e43e4d58
Jordi Gisbert Ponsoda <jordi.gisbert.ponsoda@everis.com>
2018-11-15 13:08:03 +0100

    Creando servicio HTTP

--- curso-angular4/src/app/coches/coches.component.ts
+++ curso-angular4/src/app/coches/coches.component.ts
@@ -1,21 +1,29 @@
 import { Component } from '@angular/core';
 import { Coche } from './coche';
+import { PeticionesService } from '../services/peticiones.service';
 
 @Component({
     selector: 'coches',
-    templateUrl: './coches.component.html'
+    templateUrl: './coches.component.html',
+    providers:[PeticionesService]
 })
 
 export class CochesComponent{
     public coche: Coche;
     public coches: Array<Coche>;
 
-    constructor(){
+    constructor(
+        private _peticionesService: PeticionesService;
+    ){
         this.coche = new Coche("","","");
         this.coches = [
             new Coche("Seat León","130","Rojo"),
             new Coche("Volkswagen Golf","105","Negro")
         ];
+    }
+
+    ngOnInit(){
+        console.log(this._peticionesService.getPrueba());
     }
 
     onSubmit(){
