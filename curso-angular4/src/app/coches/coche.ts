a8c6bdd811bed329e9d64c27af185e9102f16eef
Jordi Gisbert Ponsoda <jordi.gisbert.ponsoda@everis.com>
2018-11-16 13:00:49 +0100

    m

--- curso-angular4/src/app/coches/coche.ts
+++
@@ -1,8 +0,0 @@
-//ESTO ES UNA CLASE EN TYPESCRIPT, SE TRATA DEL OBJETO COCHE
-export class Coche{
-    constructor(
-        public nombre:string,
-        public caballaje:string,
-        public color:string
-){}
-}
